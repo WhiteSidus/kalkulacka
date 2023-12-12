85a786de8d4d0c4a657a2afe768319f77cd90b4b
Whiteside <vovadxc@gmail.com>
2023-12-12 13:47:33 +0100

    kalkulacka

---
+++ kalkulacka.py
@@ -0,0 +1,30 @@
+x = int(input("Zadejte první čislo: "))
+y = int(input("Zadejte druhe čislo: "))
+
+print("Pro sčítání zadejte +")
+print("Pro odčítání zadejte -")
+print("Pro násobení zadejte *")
+print("Pro dělení zadejte /")
+print("Pro mocnění zadejte **")
+print("Pro odmocnění zadejte /*")
+
+znamenko = input("Zvolte si operator: ")
+if znamenko == "+":
+    print(x + y)
+
+elif znamenko == "-":
+    print(x - y)
+
+elif znamenko == "*":
+    print(x * y)
+
+elif znamenko == "/":
+    if y != 0:
+        print(x / y)
+    else:
+        print("Chyba! Chyba! Chyba!")
+elif znamenko == "**":
+    print(x ** y)
+
+elif znamenko == "/*":
+    print(x ** (1/y))
